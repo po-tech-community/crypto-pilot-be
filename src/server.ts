7d3b5292e2d1782f3e364afaae1fda40d90ea0e5
Thanh Le <149726825+ltthanh95@users.noreply.github.com>
2025-12-08 03:41:18 -0600

    added profile and fixed token
    Added CRUD profile and fixed token

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import orderRoutes from "./modules/order/order.routes";
 import authRoutes from "./modules/authentication/auth.routes"
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
 import countryRoutes from "./modules/country/country.routes";
 import profileRoutes from "./modules/profile/profile.routes";
 
@@ -20,6 +21,8 @@ app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use("/api/accounts", accountRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/countries", countryRoutes);
+app.use("/api/profile", profileRoutes);
 app.use("/api/countries", countryRoutes);
 app.use("/api/profile", profileRoutes);
 
