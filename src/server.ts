644bfa2fd8dd12f3432ef1a8e25abc94d4132a4a
Mai Long Vuong <vmlong93@gmail.com>
2025-12-10 13:36:45 -0500

    fix: add route orders api

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 
 import express, { Request, Response } from "express";
 import accountRoutes from "./modules/account/account.routes";
+import orderRoutes from "./modules/order/order.routes";
 import authRoutes from "./modules/authentication/auth.routes"
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -15,6 +16,7 @@ app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 // Routes
 app.use("/api/accounts", accountRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/orders", orderRoutes);
 
 // Health check
 app.get("/", (req: Request, res: Response) =>
